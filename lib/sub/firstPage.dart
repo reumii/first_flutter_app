bb9d75ebb911af99689ed6b7aab06f17ec7a3768
reumii <whitestar503@naver.com>
2022-07-21 16:55:37 +0900

    ch5.탭바와 리스트

--- lib/sub/firstPage.dart
+++ lib/sub/firstPage.dart
@@ -10,21 +10,36 @@ class FirstApp extends StatelessWidget{
     return Scaffold(
       body: Container(
         child:Center(
-          child: ListView.builder(itemBuilder:(context,position){
-            return Card(
-              child: Row(
-                children:<Widget>[
-                    Image.asset(
-                      list![position].imagePath!,
-                      height:100,
-                      width:100,
-                      fit: BoxFit.contain,
+          child: ListView.builder(
+            itemBuilder:(context,position){
+              return GestureDetector(
+                child: Card(
+                  child: Row(
+                    children:<Widget>[
+                        Image.asset(
+                          list![position].imagePath!,
+                          height:100,
+                          width:100,
+                          fit: BoxFit.contain,
+                        ),
+                      Text(list![position].animalName!)
+                    ],
+                  ),
+                ),
+                onTap:(){
+                  AlertDialog dialog = AlertDialog(
+                    content: Text(
+                      '이 동물은 ${list![position].kind}입니다',
+                      style: TextStyle(fontSize:30.0),
                     ),
-                  Text(list![position].animalName!)
-                ],
-              ),
-            );
-          },
+                  );
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context)=>dialog
+                  );
+                },
+              );
+            },
             itemCount: list!.length,
           ),
         ),
--- lib/sub/secondPage.dart
+++ lib/sub/secondPage.dart
@@ -1,18 +1,113 @@
 import 'package:flutter/material.dart';
 import '../animalItem.dart';
 
-class SecondApp extends StatelessWidget{
-  final List<Animal>? list;
-  SecondApp({Key?key, this.list}) : super(key:key);
+class SecondApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => _SecondApp();
+  List<Animal>? list;
+  SecondApp({Key?key, @required this.list}) : super(key:key);
+}
+
+class _SecondApp extends State<SecondApp>{
+  final nameController = TextEditingController();
+  int? _radioValue = 0;
+  bool? flyExist = false;
+  String? _imagePath;
 
   @override
   Widget build(BuildContext context){
     return Scaffold(
-       body: Container(
-          child: Center(
-            child: Text('두 번째 페이지'),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:<Widget>[
+              TextField(
+                controller: nameController,
+                keyboardType: TextInputType.text,
+                maxLines:1,
+              ),
+              Row(
+                children:<Widget>[
+                  Radio(value:0, groupValue:_radioValue, onChanged: _radioChange),
+                  Text('양서류'),
+                  Radio(value:1, groupValue:_radioValue, onChanged: _radioChange),
+                  Text('파충류'),
+                  Radio(value:2, groupValue:_radioValue, onChanged: _radioChange),
+                  Text('포유류'),
+                ],
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+              ),
+              Row(
+                children: <Widget>[
+                  Text('날 수 있나요?'),
+                  Checkbox(
+                    value: flyExist,
+                    onChanged: (bool? check){
+                      setState((){
+                        flyExist = check;
+                      });
+                    }
+                  )
+                ],
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+              ),
+              Container(
+                height:100,
+                child:ListView(
+                  scrollDirection: Axis.horizontal,
+                  children:<Widget>[
+                    GestureDetector(
+                      child: Image.asset('repo/images/cow.png',width:80),
+                      onTap:(){
+                        _imagePath = 'repo/images/cow.png';
+                      },
+                    ),
+                    GestureDetector(
+                      child: Image.asset('repo/images/pig.png',width:80),
+                      onTap:(){
+                        _imagePath = 'repo/images/pig.png';
+                      },
+                    ),
+                    GestureDetector(
+                      child: Image.asset('repo/images/bee.png',width:80),
+                      onTap:(){
+                        _imagePath = 'repo/images/bee.png';
+                      },
+                    ),
+                    GestureDetector(
+                      child: Image.asset('repo/images/cat.png',width:80),
+                      onTap:(){
+                        _imagePath = 'repo/images/cat.png';
+                      },
+                    ),
+                    GestureDetector(
+                      child: Image.asset('repo/images/dog.png',width:80),
+                      onTap:(){
+                        _imagePath = 'repo/images/dog.png';
+                      },
+                    ),
+                    GestureDetector(
+                      child: Image.asset('repo/images/wolf.png',width:80),
+                      onTap:(){
+                        _imagePath = 'repo/images/wolf.png';
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              ElevatedButton(onPressed: (){
+
+              }, child: Text('동물 추가하기')),
+            ]
           ),
-       ),
+        ),
+      ),
     );
+  }
+  _radioChange(int? value){
+    setState((){
+      _radioValue = value;
+    });
   }
 }
