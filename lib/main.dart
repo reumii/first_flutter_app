4291e28b532704179aa2e45adc526491dc5c6841
reumii <whitestar503@naver.com>
2022-07-25 16:40:08 +0900

    ch7. 네트워크 통신

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,130 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'sub/firstPage.dart';
 import 'sub/secondPage.dart';
 import './animalItem.dart';
 import './cupertinoMain.dart';
+import 'largeFileMain.dart';
+import 'dart:convert';
 
 void main() {
-  runApp(CupertinoMain());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  static const String _title = 'Widget Example';
-
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      title: _title,
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LargeFileMain(),
     );
+  }
+}
+
+class HttpApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => _HttpApp();
+}
+
+class _HttpApp extends State<HttpApp>{
+  String result = '';
+  List? data;
+  TextEditingController? _editingController;
+  ScrollController? _scrollController;
+  int page = 1;
+
+  @override
+  void initState(){
+    super.initState();
+    data = new List.empty(growable: true);
+    _editingController = new TextEditingController();
+    _scrollController = new ScrollController();
+
+    _scrollController!.addListener(() {
+      if(_scrollController!.offset >= _scrollController!.position.maxScrollExtent && !_scrollController!.position.outOfRange){
+        print('bottom');
+        page++;
+        getJSONData();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: TextField(
+          controller:_editingController,
+          style: TextStyle(color : Colors.white),
+          keyboardType: TextInputType.text,
+          decoration: InputDecoration(hintText: '검색어를 입력하세요.'),
+        ),
+      ),
+      body: Container(
+        child: Center(
+          child: data!.length==0
+              ? Text(
+                '데이터가 없습니다.',
+                style: TextStyle(fontSize: 20),
+                textAlign: TextAlign.center)
+              : ListView.builder(
+                  itemBuilder:(context, index) {
+                    return Card(
+                      child: Container(
+                        child: Row(
+                          children: <Widget>[
+                            Image.network(
+                              data![index]['thumbnail'],
+                              height: 100,
+                              width:100,
+                              fit: BoxFit.contain,
+                            ),
+                            Column(
+                              children: <Widget>[
+                                Container(
+                                  width:MediaQuery.of(context).size.width - 150,
+                                  child: Text(
+                                    data![index]['title'].toString(),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                                Text('저자 : ${data![index]['authors'].toString()}'),
+                                Text('가격 : ${data![index]['sale_price'].toString()}'),
+                                Text('판매중 : ${data![index]['status'].toString()}'),
+                              ],
+                            ),
+                          ],
+                          mainAxisAlignment: MainAxisAlignment.start,
+                        )
+                      ),
+                    );
+                  },
+              itemCount: data!.length,
+              controller: _scrollController,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          page = 1;
+          data!.clear();
+          getJSONData();
+        },
+        child: Icon(Icons.file_download),
+      ),
+    );
+  }
+
+  Future<String> getJSONData() async{
+    var url = 'https://dapi.kakao.com/v3/search/book?target=title&page=$page&query=${_editingController!.value.text}';
+    var response = await http.get(Uri.parse(url), headers:{"Authorization":"KakaoAK ea995363d3083668e2cdde08cfe0927b"});
+
+    setState((){
+      var dataConvertedToJSON = json.decode(response.body);
+      List result = dataConvertedToJSON['documents'];
+      data!.addAll(result);
+    });
+    return response.body;
+
   }
 }
 
